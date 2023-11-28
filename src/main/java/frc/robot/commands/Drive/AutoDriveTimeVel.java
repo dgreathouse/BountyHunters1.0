e23850c9aeeac71565505927675d99c9c15a93da
dgreathouse <darryl.greathouse@gmail.com>
2023-11-28 09:41:36 -0500

    AutoDriveTimeVel RampUp and RampDown

--- src/main/java/frc/robot/commands/Drive/AutoDriveTimeVel.java
+++ src/main/java/frc/robot/commands/Drive/AutoDriveTimeVel.java
@@ -10,31 +10,36 @@ public class AutoDriveTimeVel extends Command {
   DrivetrainSubsystem m_drivetrain;
   Timer m_timer = new Timer();
   double m_rampTime = 1.0; // Seconds
-  double m_timeOut;
+  double m_timeOut_sec;
   double m_driveAngle;
   double m_robotAngle;
   double m_speed;
-  boolean m_rampEnalble = true;
+  double m_rampUpTime_sec;
+  double m_rampDownTime_sec;
   double m_currentSpeed = 0;
 
-  /** 
+  /**
    * * AutoDriveTimeVel
-   *  <p>Drive at a set velocity, robot angle and time. A velocity ramp time of 1 second will be used if rampEnable is true.
-
-   * @param _drive An instance of the drive subsystem
-   * @param _velocity The velocity you want the robot to drive at in meters/sec
-   * @param _driveAngle The Drive angle the chassis should drive at in degrees
-   * @param _robotAngle The Angle the robot should face while driving in degrees
-   * @param _timeOut The time to stop driving in seconds.
-   * @param _rampEnable Enable the ramp of velocity at the start.
+   * <p>
+   * Drive at a set velocity, robot angle and time. A velocity ramp time of 1
+   * second will be used if rampEnable is true.
+   * 
+   * @param _drive       An instance of the drive subsystem
+   * @param _velocity    The velocity you want the robot to drive at in meters/sec
+   * @param _driveAngle  The Drive angle the chassis should drive at in degrees
+   * @param _robotAngle  The Angle the robot should face while driving in degrees
+   * @param _timeOut_sec The time to stop driving in seconds.
+   * @param _rampEnable  Enable the ramp of velocity at the start.
    */
-  public AutoDriveTimeVel(DrivetrainSubsystem _drive, double _speed, double _driveAngle, double _robotAngle, double _timeOut, boolean _rampEnable) {
+  public AutoDriveTimeVel(DrivetrainSubsystem _drive, double _speed, double _driveAngle, double _robotAngle,
+      double _timeOut_sec, double _rampUpTime_sec, double _rampDownTime_sec) {
     m_drivetrain = _drive;
-    m_timeOut = _timeOut;
+    m_timeOut_sec = _timeOut_sec;
     m_driveAngle = _driveAngle;
     m_robotAngle = _robotAngle;
     m_speed = _speed;
-    m_rampEnalble = _rampEnable;
+    m_rampUpTime_sec = _rampUpTime_sec;
+    m_rampDownTime_sec = _rampDownTime_sec;
     m_currentSpeed = m_speed;
     addRequirements(m_drivetrain);
 
@@ -49,18 +54,27 @@ public class AutoDriveTimeVel extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(m_rampEnalble){m_currentSpeed = m_timer.get() < m_rampTime ? m_speed / (m_rampTime/m_timer.get()) : m_speed; }
+    double currentTime_sec = m_timer.get();
+    if (currentTime_sec < m_timeOut_sec && currentTime_sec > m_timeOut_sec - m_rampDownTime_sec) { // In the ramp down time
+      m_currentSpeed = m_speed * (m_timeOut_sec - currentTime_sec) / m_rampDownTime_sec;
+    } else if (currentTime_sec < m_rampUpTime_sec) {// In the ramp up time
+      m_currentSpeed = m_speed * currentTime_sec / m_rampUpTime_sec;
+    } else { // past the ramp up time and not in ramp down time
+      m_currentSpeed = m_speed;
+    }
+
     m_drivetrain.drivePolarFieldCentric(m_driveAngle, m_currentSpeed, m_robotAngle);
   }
-  
+
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(m_timer.hasElapsed(m_timeOut)){
+    if (m_timer.hasElapsed(m_timeOut_sec)) {
       return true;
     }
     return false;
